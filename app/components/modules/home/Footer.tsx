415dd65c847025fec6d4c9ed5e82a5e9a896c3ab
Adila Razmi <adilarazmi@Adilas-MBP.localdomain>
2023-08-29 13:02:46 +0200

    feat: add footer section

--- app/components/modules/home/Footer.tsx
+++ app/components/modules/home/Footer.tsx
@@ -1,5 +1,38 @@
+import Link from "next/link";
+import { FaGithub, FaLinkedin, FaMailBulk } from "react-icons/fa";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-navy-blue flex items-center justify-center laptop:px-[175px] py-[116px]">
+      <div className="bg-white h-[745px] w-full border-yellow border-[10px] flex flex-col justify-center items-center">
+        <div className="font-halisMedium uppercase text-blue-gray text-[32px] tracking-widest mb-[25px]">
+          Get in touch
+        </div>
+        <div className="max-w-[644px] px-4 text-center text-[20px] mb-[25px]">
+          Want to establish a connection? Just drop me an email! Or if you find
+          LinkedIn more convenient, feel free to connect with me there.
+        </div>
+        <div className="font-halisMedium text-blue-gray text-[20px] flex items-center mb-[25px] gap-2">
+          <FaMailBulk className="text-blue-gray" size={25} />
+          hello@adilarazmi.com
+        </div>
+        <div className="text-blue-gray flex items-center hover:text-gray-light gap-4">
+          <Link
+            className="text-blue-gray hover:text-gray-light"
+            href="https://www.linkedin.com/in/adila-razmi-878a01177/"
+          >
+            <FaLinkedin size={24} />
+          </Link>
+          <Link
+            className="text-blue-gray  hover:text-gray-light"
+            href="https://github.com/dila1001"
+          >
+            <FaGithub size={24} />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
